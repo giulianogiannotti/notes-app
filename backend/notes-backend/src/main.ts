8c266418432bb15d9aa10b996638c6cf6dc0cf17
giulianogiannotti <71414041+giulianogiannotti@users.noreply.github.com>
2025-08-09 09:05:32 -0300

    cors

--- backend/notes-backend/src/main.ts
+++ backend/notes-backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://notes-app-three-theta-79.vercel.app/',
   });
 
   await app.listen(3000);
