cc149d1f1e10524d03242d926727d98598eaf24a
Giuliano Giannotti <71414041+giulianogiannotti@users.noreply.github.com>
2025-08-09 14:50:50 -0300

    Update main.ts

--- backend/notes-backend/src/main.ts
+++ backend/notes-backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://notes-app-srct.vercel.app/',
+    origin: 'https://notes-app-srct.vercel.app',
   });
 
   await app.listen(3000);
